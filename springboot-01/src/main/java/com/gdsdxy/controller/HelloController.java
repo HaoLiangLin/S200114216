2fdc633dc456ad0d9d4c78cf7f7010e86bd9fd7c
lwt <2786794141@qq.com>
2022-08-29 09:35:53 +0800

    第一次提交

---
+++ springboot-01/src/main/java/com/gdsdxy/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.gdsdxy.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class HelloController {
+    @GetMapping("/helloworld")
+    public String hello() {
+        System.out.println("林武泰同学，欢迎学习springboot");
+        return "林武泰同学，欢迎学习springboot";
+    }
+}
