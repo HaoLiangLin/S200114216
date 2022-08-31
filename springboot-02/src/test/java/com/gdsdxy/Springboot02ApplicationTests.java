4d915d6c913326b336ded85c4f2a7e218089a1d5
lwt <2786794141@qq.com>
2022-08-31 11:17:53 +0800

    day2第五次提交

--- springboot-02/src/test/java/com/gdsdxy/Springboot02ApplicationTests.java
+++ springboot-02/src/test/java/com/gdsdxy/Springboot02ApplicationTests.java
@@ -3,6 +3,7 @@ package com.gdsdxy;
 import com.gdsdxy.domain.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,9 +11,13 @@ class Springboot02ApplicationTests {
     @Autowired
     private Student student;
 
+    @Value("${student.course}")
+    private String course;
+
     @Test
     void contextLoads() {
         System.out.println(student);
+        System.out.println(course);
     }
 
 }
