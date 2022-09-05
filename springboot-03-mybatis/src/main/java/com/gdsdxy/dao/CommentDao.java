34f9d19e45d3635020638d4fa97aeeaae9ecbe52
lwt <2786794141@qq.com>
2022-09-05 10:13:55 +0800

    day3第一次提交

---
+++ springboot-03-mybatis/src/main/java/com/gdsdxy/dao/CommentDao.java
@@ -0,0 +1,56 @@
+package com.gdsdxy.dao;
+
+import com.gdsdxy.pojo.Comment;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.jdbc.SQL;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface CommentDao {
+    @Insert("insert into t_comment(id, content, author, article_id) values (#{id},#{content},#{author},#{articleId})")
+    int addComment(Comment comment);
+
+    @Delete("delete from t_comment where id =#{id}")
+    int deleteComment(Integer id);
+
+    @UpdateProvider(type = SqlBuild.class, method = "update")
+    int updateComment(Comment comment);
+
+    @Select("select id,content,author,article_id from t_comment")
+    @Results({
+            @Result(column = "id", property = "id", id = true),
+            @Result(column = "content", property = "content"),
+            @Result(column = "author", property = "author"),
+            @Result(column = "article_id", property = "articleId"),
+    })
+    List<Comment> getAllComment();
+
+    @Select("select id,content,author,article_id from t_comment where id =#{id}")
+    @Results({
+            @Result(column = "id", property = "id", id = true),
+            @Result(column = "content", property = "content"),
+            @Result(column = "author", property = "author"),
+            @Result(column = "article_id", property = "articleId"),
+    })
+    Comment getCommentOne(Integer id);
+
+    class SqlBuild {
+        public String update(Comment comment) {
+            SQL sql = new SQL();
+            sql.UPDATE("t_comment");
+            if (comment.getAuthor() != null) {
+                sql.SET("author=#{author}");
+            }
+            if (comment.getContent() != null) {
+                sql.SET("content=#{content}");
+            }
+            if (comment.getArticleId() != null) {
+                sql.SET("article_id=#{articleId}");
+            }
+            return sql.toString();
+        }
+    }
+}
---
+++ springboot-03-mybatis/src/test/java/com/gdsdxy/CommentDaoTest.java
@@ -0,0 +1,38 @@
+package com.gdsdxy;
+
+import com.gdsdxy.dao.CommentDao;
+import com.gdsdxy.pojo.Comment;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class CommentDaoTest {
+    @Autowired
+    private CommentDao commentDao;
+
+    @Test
+    void addComment() {
+
+        int result = commentDao.addComment(new Comment(11, "这本书不错", "张三", 1));
+        if (result > 0) {
+            System.out.println("添加成功");
+        } else {
+            System.out.println("添加失败");
+        }
+    }
+
+    @Test
+    void selectAllTest() {
+        List<Comment> allComment = commentDao.getAllComment();
+        allComment.forEach(System.out::println);
+    }
+
+    @Test
+    void selectOneTest() {
+        Comment result = commentDao.getCommentOne(11);
+        System.out.println("result = " + result);
+    }
+}
